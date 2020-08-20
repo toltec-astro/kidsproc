a93238971832c3a40a8d626f1c719d4fa0fda310
Jerry Ma <jerry.ma.nk@gmail.com>
2020-08-20 16:56:46 -0400

    update to compute d21

--- kidsproc/kidsdata/sweep.py
+++ kidsproc/kidsdata/sweep.py
@@ -5,6 +5,8 @@
 from astropy.nddata import NDDataRef
 import astropy.units as u
 from .utils import ExtendedNDDataRef, FrequencyDivisionMultiplexingDataRef
+import numpy as np
+from tollan.utils.log import timeit
 
 
 __all__ = ['Sweep', 'MultiSweep', ]
@@ -277,3 +279,91 @@ class MultiSweep(_MultiSweepDataRef, SweepMixin):
 
         super().__init__(
                 tones=tones, sweeps=sweeps, frequency=frequency, **kwargs)
+
+    @timeit
+    def _make_d21(
+            self, flim=None, fstep=None, resample=None,
+            exclude_edge_samples=10,
+            smooth=3):
+        """The unified ``D21`` spectrum.
+
+        Parameters
+        ----------
+        flim: tuple, optional
+            If set, the d21 is resampled on to this frequency range.
+
+        fstep: float, optional
+            The step size of the frequency grid in Hz.
+
+        resample: float, optional
+            Alternative way to specify `fstep`. The frequency step
+            used will be smaller than the input data by this factor.
+
+        exclude_edge_samples: int
+            Number of samples to exclude at the tone edges.
+
+        smooth: int
+            Apply smooth to the IQs *before* the gradient operation.
+        """
+        logger = get_logger()
+        if fstep is None and resample is None:
+            fstep = 1000.
+        if not (bool(fstep is not None) ^ bool(resample is not None)):
+            raise ValueError("only one of fstep or resample can be specified")
+
+        fs = self.frequency
+        if flim is None:
+            flim = (fs.min(), fs.max())
+        fmin, fmax = flim
+        if resample is not None:
+            fstep = (fs[0, 1] - fs[0, 0]) / resample
+
+        logger.debug(
+                f"build d21 with fs=[{fmin}, {fmax}, {fstep}]"
+                f" exclude_edge_samples={exclude_edge_samples}"
+                f" original fs=[{fs.min()}, {fs.max()}]")
+        fs = np.arange(fmin, fmax, fstep)
+        adiqs0 = np.abs(self.diqs_df(self.S21, self.frequency, smooth=smooth))
+        adiqs = np.zeros(fs.shape, dtype=np.double)
+        adiqscov = np.zeros(fs.shape, dtype=int)
+        if exclude_edge_samples > 0:
+            es = slice(exclude_edge_samples, -exclude_edge_samples)
+        else:
+            es = slice(None)
+
+        for i in range(self.fs.shape[0]):
+            m = (fs >= self.fs[i].min()) & (fs <= self.fs[i].max())
+            tmp = np.interp(
+                    fs[m], self.fs[i, es], adiqs0[i, es],
+                    left=np.nan,
+                    right=np.nan,
+                    )
+            cov = ~np.isnan(tmp)
+            tmp[~cov] = 0
+            tmp[cov] += adiqs[m][cov]
+            adiqs[m] += tmp
+            adiqscov[m] += cov.astype(dtype=int)
+        m = adiqscov > 0
+        adiqs[m] /= adiqscov[m]
+        adiqs[np.isnan(adiqs)] = 0
+        d21 = Sweep(
+                S21=None, D21=adiqs, frequency=fs,
+                )
+        d21.coverage = adiqscov
+        return d21
+
+    @staticmethod
+    def diqs_df(iqs, fs, smooth=None):
+        if smooth in (None, 0):
+            pass
+        else:
+            def csmooth(arr, *args, **kwargs):
+                from scipy.ndimage.filters import uniform_filter1d
+                arr_r = uniform_filter1d(arr.real, *args, **kwargs)
+                arr_i = uniform_filter1d(arr.imag, *args, **kwargs)
+                return arr_r + 1.j * arr_i
+            iqs = csmooth(iqs, size=smooth, mode='mirror')
+        diqs = np.empty_like(iqs)
+        for i in range(iqs.shape[0]):
+            diqs[i] = np.gradient(iqs[i], fs[i])
+        return diqs
